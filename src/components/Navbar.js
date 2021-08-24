b5484f2473c70ff3c4fa7c286747d13c5f1fc2e9
grantba <grant.blaire@gmail.com>
2021-08-23 21:17:54 -0500

    Created navbar links in Navbar.js file. Added styling to Navbar.css file. Added background image and styling to App.css file. Updated styling in Footer.css file. Deleted flavicon.ico, logo512.png, and logo192.png files from public folder.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import '../css/Navbar.css'
 
 export default function Navbar() {
     return (
         <div className="navbar">
-            Navbar
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#login">Login</a></li>
+                <li><a href="#signup">Signup</a></li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+                    <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                    </div>
+                </li>
+            </ul>
         </div>
     )
 }
