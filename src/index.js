b4c19221e364a64f39cf52864ff3ee54aaa0a4cf
grantba <grant.blaire@gmail.com>
2021-08-28 21:18:09 -0500

    Installed redux and react-redux packages in app using npm install redux && npm install react-redux. Updated index.js file to import Provider from react-redux and createStore from redux libraries, added store const for the creation of the store, and wrapped App component in Provider component also adding store as props to the Provider component so that the store can be accessible in all components of the app. Created reducers folder in src folder and added rootReducer.js file to reducers folder.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/css/index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
